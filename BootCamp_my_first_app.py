fb92587ebcbfcb652926d8457501c0df5c314e09
Cerpint4xt <rcatagrundy@gmail.com>
2022-08-11 20:36:35 -0400

    Final point example 1

--- BootCamp_my_first_app.py
+++ BootCamp_my_first_app.py
@@ -11,3 +11,9 @@ st.markdown('**1. Walrus operator**')
 st.markdown('The **`Walrus` or `:=`** operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.')
 st.markdown('> Example')
 
+st.markdown('>')
+st.markdown('If we want to check and print the length of a list:')
+st.code('Mylist = [1,2,3]if(l := len(mylist) > 2)print(l)')
+
+st.markdown('> Output')
+st.markdown('>')
