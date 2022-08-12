c08ec05c5adb5516f4fd2d9ec340a86fa26a56e1
Cerpint4xt <rcatagrundy@gmail.com>
2022-08-11 20:29:51 -0400

    Trying Markdown

--- BootCamp_my_first_app.py
+++ BootCamp_my_first_app.py
@@ -6,4 +6,4 @@ st.header("10 Cool Beginner Python Tricks That Will Make Your Life Easier")
 st.subheader("Agree if you're ready to learn some Python")
 st.checkbox('I agree')
 
-
+st.markdown('_Markdown_')
