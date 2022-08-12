c725a53d3e74f499b532f3fc39dfd352da247471
Cerpint4xt <rcatagrundy@gmail.com>
2022-08-11 20:32:40 -0400

    New Markdown

--- BootCamp_my_first_app.py
+++ BootCamp_my_first_app.py
@@ -6,7 +6,7 @@ st.header("10 Cool Beginner Python Tricks That Will Make Your Life Easier")
 st.subheader("Agree if you're ready to learn some Python")
 st.checkbox('I agree')
 
-st.markdown('**1. Walrus operator**)
+st.markdown('**1. Walrus operator**')
 
 st.markdown('The **`Walrus` or `:=`** operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.')
 st.markdown('> Example')
