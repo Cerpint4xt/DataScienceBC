af07ea1f44d9dfedf3f4d32cdff06d284163f8d9
Cerpint4xt <rcatagrundy@gmail.com>
2022-08-11 20:38:01 -0400

    trying again for code display

--- BootCamp_my_first_app.py
+++ BootCamp_my_first_app.py
@@ -13,7 +13,8 @@ st.markdown('> Example')
 
 st.markdown('>')
 st.markdown('If we want to check and print the length of a list:')
-st.code('Mylist = [1,2,3]if(l := len(mylist) > 2)print(l)')
-
+st.code('Mylist = [1,2,3] > 2)')
+st.code('if(l := len(mylist)')
+st.code('print(l)')
 st.markdown('> Output')
 st.markdown('>')
