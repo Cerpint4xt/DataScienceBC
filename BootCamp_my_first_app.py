1aa8d835d9254bffe4a645ca9051e2c825eab06f
Cerpint4xt <rcatagrundy@gmail.com>
2022-08-13 14:43:44 -0400

    todos los tips and tricks

--- BootCamp_my_first_app.py
+++ BootCamp_my_first_app.py
@@ -68,5 +68,90 @@ for colour, fruits in zip(colour, fruits):
 print(colour, fruits)'''
     st.code(code9, language='python')
     st.write(output)
-    code10 = '''red appleyellow bananagreen mango'''
+    code10 = '''red apple
+yellow banana
+green mango'''
     st.code(code10, language='python')
+    st.write('''The zip() function can also be used for combining two lists into a
+dictionary. This method can be really helpful while grouping data from
+the list.  
+> Example  ''')
+    code11 = '''students = [“Rajesh”, “kumar”, “Kriti”]
+marks = [87, 90, 88]
+dictionary = dict(zip(students, marks))
+print(dictionary)'''
+    st.code(code11, language='python')
+    st.write(output)
+    code12 = '''{‘Rajesh’: 87, ‘kumar’: 90, ‘Kriti’: 88}'''
+    st.code(code12, language='python')
+    #Subtitulo Nro. 6
+    st.markdown('''### **6. Assigning multiple list values to a variable**  
+If you want to assign some specific values of a list to a variable and all
+ the remaining values to another variable in a list format, you can use
+the following technique:   
+> Example  ''')
+    code13 = '''mylist = [1,2,3,4,5]
+a,*b = mylist
+print(f”a =”,a)
+print(f”b =”,b)'''
+    st.code(code13, language='python')
+    st.write(output)
+    code14 = '''a = 1
+b = [2, 3, 4, 5]'''
+    st.code(code14, language='python')
+    st.write('''This process is also called list unpacking and you can apply this method for more than 2 variables also!  ''')
+    #Subtitulo Nro. 7
+    st.markdown('''### **7. Remove duplicate list items**  
+Do you have duplicate items in your list which you want to remove? You can do that with only one line of code using the **set()** function.  
+> Example  ''')
+    code15 = '''mylist = [1,1,1,2,2,3,3,4,4,5,6,7,7,8,9]
+newlist = set(mylist)
+print(newlist)'''
+    st.code(code15, language='python')
+    st.write(output)
+    code16 = '''{1, 2, 3, 4, 5, 6, 7, 8, 9}'''
+    st.code(code16, language='python')
+    #Subtitulo Nro. 8
+    st.markdown('''### **8. Lambda function**  
+If you need a function that is not very complicated, it can be done easily in one line using **lambda**. They are also called anonymous functions and are used heavily in data science and web development.  
+> Example  
+Let’s say you want to write a function to multiply two numbers. Instead of
+writing a conventional function, you can do that in one line using :  ''')
+    code17 = '''mul = lambda a,b: a*b
+mul(5,6)'''
+    st.code(code17, language='python')
+    st.write(output)
+    code18 = '''30'''
+    st.code(code18, language='python')
+    #Subtitulo Nro. 9
+    st.markdown('''### **9. Swapping variable value**  
+One of the first programs that we learn while learning about variables is
+swapping the values of two variables. In python you can achieve that
+with one line of code:    
+> Example  ''')
+    code19 = '''a = 100b = 200
+a,b = b,a  
+print(f’a = ‘,a)  
+print(f’b = ‘,b)'''
+    st.code(code19, language='python')
+    st.write(output)
+    code20 = '''a = 200b = 100'''
+    st.code(code20, language='python')
+    #Subtitulo Nro. 10
+    st.markdown('''### **10. Use a password in your code**  
+This python trick is amazing for securing your code with a password. We will use the **getpass() function** from the **library getpass** which encodes your input. This will prevent anyone from running the code without a password. Isn’t that cool!     
+> Example  ''')
+    code21 = '''from getpass import getpass
+password = getpass(“password: “)
+if password == “abcd”:    
+    print(“welcome strnger!”)
+else:    
+    print(“wrong password”)'''
+    st.code(code21, language='python')
+    st.write(output)
+    code22 = '''password: **** [abcd]
+**Welcome stranger!**
+Password: **** [abdc]
+Wrong password'''
+    st.code(code22, language='python')
+    st.write('''This process is also called list unpacking and you can apply this method for more than 2 variables also!  ''')
