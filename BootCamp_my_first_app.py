bbc779607ba825b9daf0a883ab1f908cf4d33be7
Cerpint4xt <rcatagrundy@gmail.com>
2022-08-13 14:06:36 -0400

    mas cambios

--- BootCamp_my_first_app.py
+++ BootCamp_my_first_app.py
@@ -9,8 +9,8 @@ if (st.checkbox('I agree')):
     In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.''')
     st.markdown('''# **1. Walrus operator**  
 The **`Walrus` or `:=`** operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.  
-> Example''')
-    st.write('''If we want to check and print the length of a list:''')
+> Example  
+If we want to check and print the length of a list:''')
     code1 = '''Mylist = [1,2,3]
 if(l := len(mylist) > 2)
 print(l)'''
